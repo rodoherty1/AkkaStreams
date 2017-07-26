35332fc9e48384dc8c3d0423a689cd75370d2776
Rob O'Doherty <rodoherty1@gmail.com>
2017-07-26 08:44:51 +0100

    Minor change to the threading unit tests

--- src/test/scala/io/rob/BrownBag3Spec.scala
+++ src/test/scala/io/rob/BrownBag3Spec.scala
@@ -4,6 +4,8 @@ import akka.NotUsed
 import akka.stream.scaladsl.{Keep, Sink, SinkQueueWithCancel, Source}
 import org.scalatest._
 
+import scala.concurrent.Future
+
 /**
   * http://akka.io/blog/2016/07/06/threading-and-concurrency-in-akka-streams-explained
   *
@@ -38,19 +40,20 @@ class BrownBag3Spec extends AsyncFlatSpec with StreamsFixture with Matchers with
 
     val source: Source[Int, NotUsed] = Source.fromIterator[Int](() => from(0).iterator)
 
-    def zipWithThreadName(i: Int): (Int, String) = (i, Thread.currentThread().getName)
-
     val sink = Sink.fold[Map[String, Int], (Int, String)](Map.empty[String, Int]) {
       case (map, (i, threadName)) =>
         val threadOccurrences = map.getOrElse(threadName, 0)
         map updated (threadName, threadOccurrences + 1)
     }
 
-    val threadnamesUsed = source
-        .take(10000)
-        .map(zipWithThreadName)
+    val threadnamesUsed: Future[Map[String, Int]] = source
+        .take(100000)
+        .map { i => (i, Thread.currentThread().getName)}
+        .mapConcat{
+          case (i, threadName) => List((i, threadName), (i, Thread.currentThread().getName))
+        }
         //. map{ case (i, threadName) => println(s"$i, $threadName"); (i, threadName)}  // Uncomment to debug
-        .runWith(sink)
+        .runWith(sink) // runWith is an alternative to "source toMat sink" followed by "run"
 
     threadnamesUsed map { threadnames =>
       printout(threadnames)
